0d6101f0ea1df050b4c92093ff569d99fa346478
sajakshrestha8 <shresthasajak123@gmail.com>
2023-08-30 22:48:23 +0545

    mail service

--- src/main/java/com/rental/bikerent/controller/UserRegister.java
+++ src/main/java/com/rental/bikerent/controller/UserRegister.java
@@ -3,7 +3,9 @@ package com.rental.bikerent.controller;
 import com.rental.bikerent.Email.EmailSenderService;
 import com.rental.bikerent.model.User;
 import com.rental.bikerent.repository.UserRepository;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -32,14 +35,28 @@ public class UserRegister {
             user.setRole("ROLE_USER");
 
             user.setPassword(PasswordEncoder.encode(user.getPassword()));
+            String otp = RandomStringUtils.randomNumeric(6);
 
             System.out.println("USER" + user);
             User result = this.userRepository.save(user);
             String to = user.getEmail();
             String subject = "Rental Bikes Welcomes!";
-            String body = "Sucessfully Resgistered";
+            String body = otp;
             System.out.printf(to);
             EmailSenderService.sendSimpleEmail(to,subject,body);
+
+//            String otp = RandomStringUtils.randomNumeric(6); // Generate a 6-digit numeric OTP
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setTo(user.getEmail());
+//            message.setSubject("Your OTP for Registration");
+//            String otp = new String();
+//            message.setText("Your OTP is: " + otp);
+//            mailSender.send(message);
+
+
+
+
+            System.out.printf(to);
 //            model.addAttribute("user", new User());
             /*session.setAttribute("message", new Message("Successfully Register!!",
                     "alert-error"));*/
